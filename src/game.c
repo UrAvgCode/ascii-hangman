67adc809cfda1356935701c672d06487b67b6fd1
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2024-07-05 15:23:42 +0200

    improve game.c

--- src/game.c
+++ src/game.c
@@ -6,142 +6,106 @@
 #include "menu.h"
 #include "sprites.h"
 
-// game constant: frames per second
-static float fps = 30;
+typedef enum { GAME_INTRO, GAME_MENU, GAME_LOOP, GAME_OVER } GAME_STATE;
 
-// game state: actual frame number
+const static float fps = 60;
+
 static int frame = 0;
-
-// game state: elapsed time
 static double elapsed = 0;
-
-// pressed key
-char key;
-
+static char key;
+static GAME_STATE game_state = GAME_INTRO;
 LANGUAGE language = ENGLISH;
 
-// definition of game phases
-typedef enum { GAME_INTRO, GAME_MENU, GAME_LOOP, GAME_OVER } GAME_STATES;
-
-// game state: actual game phase
-static GAME_STATES state = GAME_INTRO;
-
-// render a single frame
 void render_frame() {
-    int max_x;
-    int max_y;
+    int max_x, max_y;
     getmaxyx(stdscr, max_y, max_x);
     const int tx = max_x / 2;
     const int ty = max_y / 2;
 
-    // clear the actual frame
     clear();
 
-    // render the actual frame depending on the state
-    if (state == GAME_INTRO) {
-        intro(tx - 23, ty - 13);
+    switch (game_state) {
+        case GAME_INTRO:
+            intro(tx - 23, ty - 13);
+            break;
+        case GAME_MENU:
+            drawMenu(tx - 13, ty - 13);
+            break;
+        case GAME_LOOP:
+        case GAME_OVER:
+            drawHangman(tx - 9, ty, getMistakes());
+            drawCactus(tx - 35, ty + 4, 0);
+            drawCactus(tx + 27, ty + 9, 1);
+            drawFloor(0, ty + 17, max_x);
+            drawHintLetters(10, 7);
 
-    } else if (state == GAME_MENU) {
-        drawMenu(tx - 13, ty - 13);
-
-    } else if (state == GAME_LOOP || state == GAME_OVER) {
-
-        // Draw Graphics
-        drawHangman(tx - 9, ty, getMistakes());
-        drawCactus(tx - 35, ty + 4, 0);
-        drawCactus(tx + 27, ty + 9, 1);
-        drawFloor(0, ty + 17, max_x);
-        drawHintLetters(10, 7);
-
-        if (state == GAME_LOOP) {
-            drawHintWord(10, 5);
-            drawBird((frame / 2) % max_x, 12);
-            updateHintWord(key);
-        } else {
-            drawGuessWord(10, 5);
-            drawGameOver(tx - 20, 10, getHasWon());
-        }
+            if (game_state == GAME_LOOP) {
+                drawHintWord(10, 5);
+                drawBird((frame / 2) % max_x, 12);
+                updateHintWord(key);
+            } else {
+                drawGuessWord(10, 5);
+                drawGameOver(tx - 20, 10, getHasWon());
+            }
+            break;
     }
 
     refresh();
 }
 
-// update the game state
-// * esc goes back to menu
 bool update_state() {
-    // state check cascade
-    switch (state) {
+    switch (game_state) {
         case GAME_INTRO:
             if (elapsed > 3)
-                state = GAME_MENU;
+                game_state = GAME_MENU;
             break;
         case GAME_MENU:
             switch (updateMenuState(key)) {
-                case 0:
+                case START_GAME:
                     reset();
                     pickGuessWord(language);
                     createHintWord();
-                    state = GAME_LOOP;
+                    game_state = GAME_LOOP;
                     break;
-                case 1:
+                case CHANGE_LANGUAGE:
                     language = !language;
                     break;
-                case 2:
-                    return true;
-                default:
-                    break;
+                case QUIT_GAME:
+                    return false;
             }
             break;
         case GAME_LOOP:
             if (isGameOver())
-                state = GAME_OVER;
+                game_state = GAME_OVER;
             break;
         case GAME_OVER:
             break;
     }
 
-    if (key == 27)
-        state = GAME_MENU;
+    if (key == 27) {
+        game_state = GAME_MENU;
+    }
 
-    return (false);
+    return true;
 }
 
-// the game loop
-// * it renders frame after frame
-// * updates the state after each frame
-// * and sleeps for the remainder of the frame
 void game_loop() {
-    const float dt = 1 / fps; // frame duration
-    const float ms = dt * 1000; // milli seconds
+    const float delta_time = 1.0f / fps;
+    const float millis = delta_time * 1000.0f;
 
-    // initialize ncurses screen
-    initscr(); // initialize the screen to contain a single window
-    curs_set(FALSE); // disable text cursor
-    timeout(0); // configure getch() to be non-blocking
-    keypad(stdscr, TRUE); // configure getch() to return cursor key codes
-    noecho(); // do not echo keyboard input
+    initscr();
+    curs_set(false);
+    timeout(0);
+    keypad(stdscr, true);
+    noecho();
 
-    // the main game loop
-    while (true) {
-
-        // keyboard input
+    while (update_state()) {
         key = getch();
 
-        // render a single frame
         render_frame();
-
-        // update frame counter and elapsed time
         frame++;
-        elapsed += dt;
+        elapsed += delta_time;
 
-        // update the game state
-        const bool finish = update_state();
-
-        // sleep for the remainder of the frame
-        usleep(ms * 1000);
-
-        // check for game finish
-        if (finish)
-            break;
+        usleep(millis * 1000.0f);
     }
 }
